422200cc290ab2640671e5d587d9476db9bf3bec
Sameer Rahmani <lxsameer@gnu.org>
2015-07-12 01:49:36 +0430

    MID1 project-manager.el

---
+++ lib/fg42/project-manager.el
@@ -0,0 +1,8 @@
+;; Vars ------------------------
+(defvar fg42-default-project-file ".fg42.json"
+  "The name of default project file in projects.")
+
+;; Functions -------------------
+(defun fg42-read-project-data ())
+
+(defun fg42-write-project-data ())
