8c1b6a5af59a2147f507109405f5328ec39904ab
amirhoshangi <amirhoshangi@gmail.com>
2018-12-28 19:02:52 +0330

    latex.el: implement company-aux package using fg42 extension notation

---
+++ lib/extensions/latex.el
@@ -0,0 +1,16 @@
+(require 'fpkg)
+(require 'fg42/extension)
+(require 'extensions/latex/init)
+
+;; Dependencies ----------------------------------
+(depends-on 'company-auctex)
+
+(defun latex-doc ()
+  "something fun")
+;; Extension -------------------------------------
+(extension latex
+     :version "2."
+     :on-initialize extensions/latex-initialize
+           :docs "lib/extensions/latex/readme.org")
+
+(provide 'extensions/latex)
