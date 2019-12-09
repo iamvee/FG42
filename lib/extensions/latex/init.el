0fddfe89478cebbc5d29021afe06f13563eafc9b
Sameer Rahmani <lxsameer@gnu.org>
2019-12-09 21:23:35 +0000

    Latex ability has been improved

--- lib/extensions/latex/init.el
+++ lib/extensions/latex/init.el
@@ -1,12 +1,12 @@
 ;;;###autoload
 (defun extensions/latex-initialize ()
-"Latex autocompletion support."
-	   (require 'extensions/latex/lsp-latex)
-	   (with-eval-after-load "tex-mode"
-	     (add-hook 'latex-mode-hook 'latex-mode-hook
-		       '(lambda()
-			  (ability lsp-latex ('lsp)
-				   (setq lsp-latex-texlab-executable "/usr/bin/texlab")
-				   (lsp))))))
+  "Latex autocompletion support."
+  (ability lsp-latex ('lsp)
+           (with-eval-after-load "tex-mode"
+             (add-hook 'latex-mode-hook 'latex-mode-hook
+	               '(lambda()
+                          (require 'extensions/latex/lsp-latex)
+		          (setq lsp-latex-texlab-executable "/usr/bin/texlab")
+		          (lsp))))))
 
 (provide 'extensions/latex/init)
