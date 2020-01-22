cb3d8866183b459df9ebc687a298bdc15e66c17e
Sameer Rahmani <lxsameer@gnu.org>
2020-01-22 23:36:56 +0000

    guix/guix-daemon installation has been done

--- scripts/install_guix.sh
+++ scripts/install_guix.sh
@@ -29,21 +29,18 @@ guix_bin=$(dirname $guix_path | sed 's/^.//')
 
 daemon_path=`find . -iname "guix-daemon" | grep bin | head -1`
 daemon_bin=$(dirname $guix_path | sed 's/^.//')
+
+
+mkdir -p root/.config/guix/
 popd
 
-# local -r bash_bin=$(dirname `find "$OUTPUTDIR/gnu/store" -iname "bash" | grep bin | head -1`)
+pushd $guix_dir/root/.config/guix/
+ln -sf ../../../../var/guix/profiles/per-user/root/current-guix ./current
+popd
 
-# echo "###"
-# echo $bash_bin
-# local -r guix_path=`find "$OUTPUTDIR/gnu/store" -iname "guix-daemon" | grep bin | head -1`
-# local -r guix_bin=$(dirname $guix_path)
+# Path related to rootfs
+PROFILE_DIR=/root/.config/guix/current/
 
-# ROOT_DIR=$OUTPUTDIR/root
-# mkdir -p $ROOT_DIR/.config/guix/current
-
-# ln -sf $OUTPUTDIR/var/guix/profiles/per-user/root/current-guix $ROOT_DIR/.config/guix/current
-# ln -sf $bash_bin/bash $OUTPUTDIR/bin/bash
-#$guix_dir/var/guix/profiles/per-user/$(whoami)/current-guix
 #GUIX_PROFILE=$guix_dir/var/guix/profiles/per-user/$(whoami)/current-gui
 
 #GUIX_PROFILE=$ROOT_DIR/.config/guix/current
@@ -53,15 +50,15 @@ export PATH="${bindir}:${guix_bin}:${PATH}"
 {
     echo "#!/usr/bin/env bash"
     echo
-    echo "proot -r ${guix_dir} -b /dev -b /proc -b /sys --root-id $guix_bin/guix \$@"
+    echo "proot -R ${guix_dir} -w / -b /dev -b /proc -b /sys -b /tmp:/tmp --root-id $PROFILE_DIR/bin/guix \$@"
 } > "${bindir}/guix"
 chmod u+x "${bindir}/guix"
 
 {
     echo "#!/usr/bin/env bash"
     echo
-    echo "proot -R ${guix_dir} -w / -b /dev -b /proc -b /sys -b /tmp:/tmp --root-id $daemon_bin/guix-daemon --build-users-group=\$(whoami)"
-    # /root/.config/guix/current/bin/guix-daemon --build-users-group=\$(whoami)
+    echo 'echo $(whoami)'
+    echo "proot -R ${guix_dir} -w / -b /dev -b /proc -b /sys -b /tmp:/tmp --root-id $PROFILE_DIR/bin/guix-daemon --build-users-group=\$(whoami) \$@"
 } > "${bindir}/guix-daemon"
 chmod u+x "${bindir}/guix-daemon"
 
